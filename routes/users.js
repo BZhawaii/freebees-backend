9c226e21fa5e64c00175093ff21ec68b37a46e2c
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-04-02 21:43:36 -0600

    Added full crud for users

--- routes/users.js
+++ routes/users.js
@@ -21,9 +21,45 @@ router.get('/', function(req, res, next) {
     })
 });  //closes router.get
 
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id;
+  if(typeof id != 'undefined') {
+    knex('users')
+    .select()
+    .where('id', id)
+    .first()
+    .then(user => {
+      res.render('usersSingle', user);
+    })
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid id'
+    })
+  }
+});  //closes router.get
+
 router.get('/new', function(req, res, next) {
   res.render('newForm');
 });  //closes router.get
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id;
+  if(typeof id != 'undefined') {
+    knex('users')
+    .select()
+    .where('id', id)
+    .first()
+    .then(user => {
+      res.render('usersEdit', user);
+    })
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid id'
+    })
+  }
+})  //closes router.get
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
@@ -49,6 +85,48 @@ router.post('/', function(req, res, next) {
     });
   }
 });  //closes router.get
+
+router.put('/:id', (req, res) => {
+  console.log(req.body);
+  if(validForm(req.body)) {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      sex: req.body.sex
+    }
+    knex('users')
+    .where('id', req.params.id)
+      .update(user, 'id')
+      .then(ids => {
+        res.redirect(`/users/${req.params.id}`);
+      })
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid user'
+    });
+  }
+});  //closes router.put
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  if(typeof id != 'undefined') {
+    knex('users')
+      .where('id', id)
+      .del()
+      .then(() => {
+        res.redirect('/users')
+      })
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid id'
+    });
+  }
+})  //closes router.delete
 
 
 module.exports = router;
