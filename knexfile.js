9a35bbb5949907cfd8200c9845122817005a2911
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-04-09 09:05:31 -0600

    Modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-// require('dotenv').config({silent:true});
+require('dotenv').config({silent:true});
 
 
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: 'postgresql',
     connection: 'postgres://localhost/freebees'
   },  //closes development
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true'
   }  //closes productions
 };   //closes module.exports
